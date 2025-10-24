03d04d113f8439558e50f7827fb9510ab7a27be4
cybergirl-1 <shraddhashukla846@gmail.com>
2025-10-24 11:01:49 +0530

    Create INTRO.C to display name and address
    Add a simple C program that prints a name and address.

---
+++ INTRO.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+printf("My name is kailash Punjabi.\n");
+printf("I reside at : \t 109 prem bhuvan,");
+printf("\n \t \t Bazar Road,");
+printf("\n \t\t Bandra west,");
+printf("\n\t\t Mumbai 400050");
+getch();
+}
